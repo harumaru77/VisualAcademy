547c3317575de04f1f8e66b8098bb595db9d4871
KwonWoo, Nam <kwonwoo77@gmail.com>
2020-07-07 13:25:33 +0900

    Javascript Interop를 사용하여 Bootstrap Carousel 실행되도록 하기

--- VisualAcademy/Pages/Index.razor.cs
+++ VisualAcademy/Pages/Index.razor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +10,16 @@ namespace VisualAcademy.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
         protected override void OnInitialized()
         {
             
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeAsync<Object>("RunCarousel");
         }
     }
 }
